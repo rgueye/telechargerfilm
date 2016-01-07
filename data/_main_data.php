5c912c443c25b9378b4907aa2c4e0f7c03a7a8aa
agarand <alexandre_307@hotmail.com>
2016-01-06 22:46:14 -0500

    Début authentification de session

--- data/_main_data.php
+++ data/_main_data.php
@@ -1,3 +1,10 @@
 <?php
+session_start();
 //$site_data = array(); // Les variables du site
 $site_data[USER_IS_LOGGED] = false; //L'utilisateur n'est pas connecté par défaut
+
+
+//Si user_is_logged est dans le tableau de la session et que sa valeur est égale à true
+if (array_key_exists(USER_IS_LOGGED, $_SESSION) && $_SESSION[USER_IS_LOGGED] == true) {
+    $site_data[USER_IS_LOGGED] = true;
+}
--- view_parts/_login_out_form.php
+++ view_parts/_login_out_form.php
@@ -1,9 +1,35 @@
-<?php ?>
+<?php
+
+$error = false;
+if (array_key_exists('username', $_POST) && array_key_exists('password', $_POST)) {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+
+    //C'est ici qu'on va venir dans la base de données que l'utilisateur existe
+    if ($username == "dieu" && $password == "qwerty") {
+        $_SESSION[USER_IS_LOGGED] = true;
+    } else {
+        $error = true;
+    }
+}
+if (array_key_exists('dologout', $_POST)) {
+
+    session_destroy();
+}
+?>
 
 <?php if(! $site_data[USER_IS_LOGGED]) {
 //Si l'utilisateur
     ?>
-    <form id="login" name="login">
+    <form id="login" method="post" action="/" name="login">
+
+        <?php if ($error == true) { ?>
+            <p style="color: red">Connexion échouée</p>
+        <?php } ?>
+
 
         <label for="username">Pseudo : </label>
         <input type="text" name="username" id="username" value=""/>
@@ -12,7 +38,7 @@
         <input type="submit" name="dologin" id="dologin" value="Entrer"/>
     </form>
 <?php } else { //Si l'utilisateur est connecté ?>
-    <form id="logout" name="logout">
+    <form id="logout" method="post" action="/" name="logout">
         <input type="submit" name="dologout" value="Quitter"/>
     </form>
 <?php } ?>
