90b12a0c8de8e8ba227b3d6f411efb76137f3d82
agarand <alexandre_307@hotmail.com>
2016-01-15 15:54:12 -0500

    listes fonctionnent, mais les images ne s'affichent pas encore -_-

--- catalogue_action.php
+++ catalogue_action.php
@@ -12,124 +12,126 @@ $donnees = array(
     ),
 
     '2' => array(
-        'source' => 'images/images action/557854.jpg-r_160_240-b_1_D6D6D6-f_jpg-q_x-xxyxx.jpg>'
-    ),
+        'source' => 'images/images action/557854.jpg-r_160_240-b_1_D6D6D6-f_jpg-q_x-xxyxx.jpg>',
         'lien' => '',
         'alt' => '1',
+    ),
 
     '3' => array(
-        'source' =>  'images/images action/images (2).jpg>'
-    ),
+        'source' =>  'images/images action/images (2).jpg>',
         'lien' => '',
         'alt' => '2',
+    ),
 
     '4' => array(
-        'source' => 'images/images action/images (4).jpg>'
-    ),
+        'source' => 'images/images action/images (4).jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '5' => array(
-        'source' => 'images/images action/images (5).jpg>'
-    ),
+        'source' => 'images/images action/images (5).jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '6' => array(
-        'source' => 'images/images action/images (6).jpg>'
-    ),
+        'source' => 'images/images action/images (6).jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '7' => array(
-        'source' => 'images/images action/372128.jpg-r_160_240-b_1_D6D6D6-f_jpg-q_x-xxyxx.jpg>'
-    ),
+        'source' => 'images/images action/372128.jpg-r_160_240-b_1_D6D6D6-f_jpg-q_x-xxyxx.jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '8' => array(
-        'source' => 'images/images action/images (8).jpg>'
-    ),
+        'source' => 'images/images action/images (8).jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '9' => array(
-        'source' => 'images/images action/images (9).jpg>'
-    ),
+        'source' => 'images/images action/images (9).jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '10' => array(
-        'source' => 'images/images action/images (10).jpg>'
-    ),
+        'source' => 'images/images action/images (10).jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
+
 
     '11' => array(
-        'source' => 'images/images action/images (11).jpg>'
-    ),
+        'source' => 'images/images action/images (11).jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '12' => array(
-        'source' => 'images/images action/images (12).jpg>'
-    ),
+        'source' => 'images/images action/images (12).jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '13' => array(
-        'source' => 'images/images action/téléchargement (7).jpg>'
-    ),
+        'source' => 'images/images action/téléchargement (7).jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '14' => array(
-        'source' => 'images/images action/téléchargement (3).jpg>'
-    ),
+        'source' => 'images/images action/téléchargement (3).jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '15' => array(
-        'source' => 'images/images action/téléchargement (6).jpg>')
-,
+        'source' => 'images/images action/téléchargement (6).jpg>',
         'lien' => '',
-        'alt' => 'image indisponible',
+        'alt' => 'image indisponible'
+    ),
 
     '16' => array(
-        'source' => 'images/images action/téléchargement (1).jpg>')
-,
+        'source' => 'images/images action/téléchargement (1).jpg>',
         'lien' => '',
-        'alt' => 'image indisponible',
+        'alt' => 'image indisponible'
+    ),
 
     '17' => array(
-        'source' => 'images/images action/téléchargement (2).jpg>')
-,
+        'source' => 'images/images action/téléchargement (2).jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '18' => array(
-        'source' => 'images/images action/téléchargement.jpg>')
-,
+        'source' => 'images/images action/téléchargement.jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '19' => array(
-        'source' => 'images/images action/085787.jpg-r_160_240-b_1_D6D6D6-f_jpg-q_x-xxyxx.jpg>'
-    ),
+        'source' => 'images/images action/085787.jpg-r_160_240-b_1_D6D6D6-f_jpg-q_x-xxyxx.jpg>',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '20' => array(
-        'source' => 'images/images action/261485.jpg-r_160_240-b_1_D6D6D6-f_jpg-q_x-xxyxx.jpg'
-    ),
+        'source' => 'images/images action/261485.jpg-r_160_240-b_1_D6D6D6-f_jpg-q_x-xxyxx.jpg',
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
     '21' => array(
-        'source' => 'images/images action/images.jpg'
-    ),
+        'source' => 'images/images action/images.jpg',
+
         'lien' => '',
         'alt' => 'image indisponible',
+    ),
 
 
 //\'detail_sicario.php\'
